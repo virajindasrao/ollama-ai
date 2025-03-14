f2a3009e0e2b3c716087f101342af3a4bd1785ee
virajindasrao <virajindasrao@gmail.com>
2025-03-14 16:45:23 +0800

    libs assistant system prompt

---
+++ libs/assistant_helper.py
@@ -0,0 +1,23 @@
+"""script to maintain ollama helper functions centrally"""
+import ollama
+
+class Assistant():
+    """Ollama assistant helper class"""
+    def __init__(self, model, system_prompt=None):
+        # Set model initial values
+        self.model = model
+        self.system_prompt = system_prompt
+        # Initiate ollam client
+        self.c = ollama.Client()
+
+    def chat(self):
+        """function to call ollama chat function"""
+
+    def generate(self, prompt):
+        """function to generaet content from ollama"""
+        response = self.c.generate(
+            model = self.model,
+            prompt = prompt,
+            system = self.system_prompt
+        )
+        return response.response
