03521ad7577fac4bc72583f9031ec01b6e68b59f
virajindasrao <virajindasrao@gmail.com>
2025-05-17 12:38:34 +0800

    05 custom model:install.sh

---
+++ 05_custom_model/install.sh
@@ -0,0 +1,5 @@
+yum install python3-pip -y
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118
+pip3 install -r requirements.txt
