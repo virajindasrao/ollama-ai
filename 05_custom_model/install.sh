56543f1d7d42b246a99318bf9eb50194d4677bf8
virajindasrao <virajindasrao@gmail.com>
2025-05-17 14:54:11 +0800

    05 custom model:install script

--- 05_custom_model/install.sh
+++ 05_custom_model/install.sh
@@ -1,5 +1,6 @@
-yum install python3-pip -y
-pip3 install --upgrade pip
+sudo yum install python3-pip -y
+sudo yum install pip -y
+pip install --upgrade pip
 pip uninstall torch torchvision -y
 pip install torch==2.0.1+cpu torchvision==0.15.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install -r requirements.txt
+pip install -r requirements.txt
