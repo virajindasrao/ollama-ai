68d67eab18568e8948b97d9b5c17b8d885cdb836
virajindasrao <virajindasrao@gmail.com>
2025-05-17 15:41:17 +0800

    05 custom model:mem optimized

--- 05_custom_model/install.sh
+++ 05_custom_model/install.sh
@@ -5,3 +5,5 @@ pip uninstall torch torchvision -y
 pip install torch==2.0.1+cpu torchvision==0.15.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 python3 05_custom_model/fine_tune_model.py --model_name mistralai/Mistral-7B-v0.1 --data_path 05_custom_model/train-data.json --output_dir 05_custom_model/fine_tuned_model --epochs 3 --batch_size 8 --learning_rate 5e-5
+pip install transformers==4.49.0
+pip install psutil
