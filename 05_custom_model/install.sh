8230ee530e47afcc85736002681513dce7c430b5
virajindasrao <virajindasrao@gmail.com>
2025-05-19 10:48:52 +0800

    05 custom model:package upgrade

--- 05_custom_model/install.sh
+++ 05_custom_model/install.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 python3 05_custom_model/fine_tune_model.py --model_name mistralai/Mistral-7B-v0.1 --data_path 05_custom_model/train-data.json --output_dir 05_custom_model/fine_tuned_model --epochs 3 --batch_size 8 --learning_rate 5e-5
 pip install transformers==4.49.0
 pip install psutil
+pip install numpy==1.24.3
+pip install pybind11>=2.12
